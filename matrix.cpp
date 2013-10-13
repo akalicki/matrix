676652c46629116cf0ab619d0147f41438464003
Alex Kalicki <alexander.kalicki@gmail.com>
2013-10-13 13:02:15 -0400

    Added project files

---
+++ matrix.cpp
@@ -0,0 +1,282 @@
+/*
+ * matrix.cpp
+ */
+
+#include <stdexcept>
+#include "matrix.h"
+
+using std::ostream;  using std::istream;  using std::endl;
+using std::domain_error;
+
+/* PUBLIC MEMBER FUNCTIONS
+ ********************************/
+
+Matrix::Matrix(int rows, int cols) : rows_(rows), cols_(cols)
+{
+    allocSpace();
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = 0;
+        }
+    }
+}
+
+Matrix::Matrix() : rows_(1), cols_(1)
+{
+    allocSpace();
+    p[0][0] = 0;
+}
+
+Matrix::~Matrix()
+{
+    for (int i = 0; i < rows_; ++i) {
+        delete[] p[i];
+    }
+    delete[] p;
+}
+
+Matrix::Matrix(const Matrix& m) : rows_(m.rows_), cols_(m.cols_)
+{
+    allocSpace();
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = m.p[i][j];
+        }
+    }
+}
+
+Matrix& Matrix::operator=(const Matrix& m)
+{
+    if (this == &m) {
+        return *this;
+    }
+
+    if (rows_ != m.rows_ || cols_ != m.cols_) {
+        for (int i = 0; i < rows_; ++i) {
+            delete[] p[i];
+        }
+        delete[] p;
+
+        rows_ = m.rows_;
+        cols_ = m.cols_;
+        allocSpace();
+    }
+
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix& Matrix::operator+=(const Matrix& m)
+{
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] += m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix& Matrix::operator-=(const Matrix& m)
+{
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] -= m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix& Matrix::operator*=(const Matrix& m)
+{
+    Matrix temp(rows_, m.cols_);
+    for (int i = 0; i < temp.rows_; ++i) {
+        for (int j = 0; j < temp.cols_; ++j) {
+            for (int k = 0; k < cols_; ++k) {
+                temp.p[i][j] += (p[i][k] * m.p[k][j]);
+            }
+        }
+    }
+    return (*this = temp);
+}
+
+Matrix& Matrix::operator*=(double num)
+{
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] *= num;
+        }
+    }
+    return *this;
+}
+
+Matrix Matrix::operator^(int num)
+{
+    Matrix temp(*this);
+    return expHelper(temp, num);
+}
+
+/* STATIC CLASS FUNCTIONS
+ ********************************/
+
+Matrix Matrix::createIdentity(int size)
+{
+    Matrix temp(size, size);
+    for (int i = 0; i < temp.rows_; ++i) {
+        for (int j = 0; j < temp.cols_; ++j) {
+            if (i == j) {
+                temp.p[i][j] = 1;
+            } else {
+                temp.p[i][j] = 0;
+            }
+        }
+    }
+    return temp;
+}
+
+Matrix Matrix::solve(Matrix A, Matrix b)
+{
+    // Gaussian elimination
+    for (int i = 0; i < A.rows_; ++i) {
+        if (A.p[i][i] == 0) {
+            // pivot 0 - throw error
+            throw domain_error("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
+        }
+        for (int j = i + 1; j < A.rows_; ++j) {
+            for (int k = i + 1; k < A.cols_; ++k) {
+                A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
+            }
+            b.p[j][0] -= b.p[i][0] * (A.p[j][i] / A.p[i][i]);
+            A.p[j][i] = 0;
+        }
+    }
+
+    // Back substitution
+    Matrix x(b.rows_, 1);
+    x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
+    for (int i = x.rows_ - 2; i >= 0; --i) {
+        int sum = 0;
+        for (int j = i + 1; j < x.rows_; ++j) {
+            sum += A.p[i][j] * x.p[j][0];
+        }
+        x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
+    }
+
+    return x;
+}
+
+Matrix Matrix::bandSolve(Matrix A, Matrix b, int k)
+{
+    // optimized Gaussian elimination
+    int bandsBelow = (k - 1) / 2;
+    for (int i = 0; i < A.rows_; ++i) {
+        if (A.p[i][i] == 0) {
+            // pivot 0 - throw exception
+            throw domain_error("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
+        }
+        for (int j = i + 1; j < A.rows_ && j <= i + bandsBelow; ++j) {
+            int k = i + 1;
+            while (k < A.cols_ && A.p[j][k]) {
+                A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
+                k++;
+            }
+            b.p[j][0] -= b.p[i][0] * (A.p[j][i] / A.p[i][i]);
+            A.p[j][i] = 0;
+        }
+    }
+
+    // Back substitution
+    Matrix x(b.rows_, 1);
+    x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
+    for (int i = x.rows_ - 2; i >= 0; --i) {
+        int sum = 0;
+        for (int j = i + 1; j < x.rows_; ++j) {
+            sum += A.p[i][j] * x.p[j][0];
+        }
+        x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
+    }
+
+    return x;
+}
+
+
+/* PRIVATE HELPER FUNCTIONS
+ ********************************/
+
+void Matrix::allocSpace()
+{
+    p = new double*[rows_];
+    for (int i = 0; i < rows_; ++i) {
+        p[i] = new double[cols_];
+    }
+}
+
+Matrix Matrix::expHelper(const Matrix& m, int num)
+{
+    if (num == 0) { 
+        return createIdentity(m.rows_);
+    } else if (num == 1) {
+        return m;
+    } else if (num % 2 == 0) {  // num is even
+        return expHelper(m * m, num/2);
+    } else {                    // num is odd
+        return m * expHelper(m * m, (num-1)/2);
+    }
+}
+
+/* NON-MEMBER FUNCTIONS
+ ********************************/
+
+Matrix operator+(const Matrix& m1, const Matrix& m2)
+{
+    Matrix temp(m1);
+    return (temp += m2);
+}
+
+Matrix operator-(const Matrix& m1, const Matrix& m2)
+{
+    Matrix temp(m1);
+    return (temp -= m2);
+}
+
+Matrix operator*(const Matrix& m1, const Matrix& m2)
+{
+    Matrix temp(m1);
+    return (temp *= m2);
+}
+
+Matrix operator*(const Matrix& m, double num)
+{
+    Matrix temp(m);
+    return (temp *= num);
+}
+
+Matrix operator*(double num, const Matrix& m)
+{
+    return (m * num);
+}
+
+ostream& operator<<(ostream& os, const Matrix& m)
+{
+    for (int i = 0; i < m.rows_; ++i) {
+        os << m.p[i][0];
+        for (int j = 1; j < m.cols_; ++j) {
+            os << " " << m.p[i][j];
+        }
+        os << endl;
+    }
+    return os;
+}
+
+istream& operator>>(istream& is, Matrix& m)
+{
+    for (int i = 0; i < m.rows_; ++i) {
+        for (int j = 0; j < m.cols_; ++j) {
+            is >> m.p[i][j];
+        }
+    }
+    return is;
+}
---
+++ matrix.h
@@ -0,0 +1,46 @@
+/*
+ * matrix.h
+ */
+
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+#include <iostream>
+
+class Matrix {
+    public:
+        Matrix(int, int);
+        Matrix();
+        ~Matrix();
+        Matrix(const Matrix&);
+        Matrix& operator=(const Matrix&);
+
+        inline double& operator()(int x, int y) { return p[x][y]; }
+
+        Matrix& operator+=(const Matrix&);
+        Matrix& operator-=(const Matrix&);
+        Matrix& operator*=(const Matrix&);
+        Matrix& operator*=(double);
+        Matrix  operator^(int);
+        
+        friend std::ostream& operator<<(std::ostream&, const Matrix&);
+        friend std::istream& operator>>(std::istream&, Matrix&);
+
+        static Matrix createIdentity(int);
+        static Matrix solve(Matrix, Matrix);
+        static Matrix bandSolve(Matrix, Matrix, int);
+    private:
+        int rows_, cols_;
+        double **p;
+
+        void allocSpace();
+        Matrix expHelper(const Matrix&, int);
+};
+
+Matrix operator+(const Matrix&, const Matrix&);
+Matrix operator-(const Matrix&, const Matrix&);
+Matrix operator*(const Matrix&, const Matrix&);
+Matrix operator*(const Matrix&, double);
+Matrix operator*(double, const Matrix&);
+
+#endif
